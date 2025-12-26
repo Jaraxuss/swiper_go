b4baa0e89a712b8276a4f7f14473ad5057e044af
velen <Jaraxuss@live.com>
2025-12-26 19:14:04 +0800

    feat: externalize Swiper slide configurations to `config.js` and update `index.html` to dynamically generate slides using the new configuration.

---
+++ js/config.js
@@ -0,0 +1,20 @@
+/**
+ * Slide Show Configuration
+ * Add or remove slides here. Supports comments and easy reordering.
+ */
+const slideConfig = [
+    "pages/1 宣讲会背景及目的.html",
+    "pages/2 启动会首页.html",
+    "pages/3 院长致辞.html",
+    "pages/7 影刀能做什么.html",
+    "pages/8 影刀能做什么2.html",
+    "pages/9 小试影刀.html",
+    "pages/12 更多场景案例1.html",
+    "pages/直播自动化.html",
+
+    // Example of a commented out slide:
+    // "pages/future-slide.html",
+];
+
+// If you need to export this for potential build tools, you can add:
+// if (typeof module !== 'undefined') module.exports = slideConfig;
