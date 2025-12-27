2a8988f7a472661cbf9086535bb081f16bfb22c5
velen <Jaraxuss@live.com>
2025-12-27 22:20:31 +0800

    1. Add 小红书达人批量采集.html 2. rename page

--- js/config.js
+++ js/config.js
@@ -4,17 +4,23 @@
  */
 const slideConfig = [
     "pages/启动会首页.html",
+
     "pages/影刀RPA简介.html",
     "pages/影刀能做什么.html",
     "pages/影刀能做什么2.html",
+
     "pages/小试影刀.html",
+
     "pages/核心场景相关案例1.html",
     "pages/小红书笔记批量采集.html",
     "pages/影刀AI Power介绍.html",
     "pages/AI音频克隆.html",
-    "pages/核心场景相关案例2.html",
-    "pages/核心场景相关案例3.html",
+
+    "pages/更多场景案例1.html",
+    "pages/小红书达人批量采集.html",
     "pages/直播自动化.html",
+
+    "pages/更多场景案例2.html",
 ];
 
 // If you need to export this for potential build tools, you can add:
