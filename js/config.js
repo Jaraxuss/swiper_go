d20d6b9ef1e2cc0a4f9431758de697d8f69a7a4e
velen <Jaraxuss@live.com>
2025-12-27 23:33:11 +0800

    1. Add 舆情监测自动化.html 2. Add 财务部门案例

--- js/config.js
+++ js/config.js
@@ -19,8 +19,11 @@ const slideConfig = [
     "pages/更多场景案例1.html",
     "pages/小红书达人批量采集.html",
     "pages/直播自动化.html",
+    "pages/舆情监测自动化.html",
 
     "pages/更多场景案例2.html",
+    "pages/场景_财务部痛点.html",
+    "pages/场景_财务部方案.html",
 ];
 
 // If you need to export this for potential build tools, you can add:
