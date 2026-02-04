34747252f03792d99a27f10329e6520e8f79a7b3
velen <Jaraxuss@live.com>
2026-02-04 23:09:50 +0800

    update config

--- js/config.js
+++ js/config.js
@@ -22,57 +22,56 @@ const slideConfig = [
 
     // 行业挑战和 RPA 价值
     "pages/custom/RPA制造业挑战.html",
-    "pages/custom/RPA制造业场景列表.html",
-    "pages/custom/RPA制造业间接价值.html",
-    "pages/custom/RPA制造业直接价值.html",
-
-    // 制造业数字化与RPA场景
-    "pages/custom/制造业的数字化地图.html",
     "pages/custom/业务挑战_流程断点多海量非结构数据.html",
     "pages/custom/制造业RPA场景地图.html",
-    "pages/custom/RPA+AI文件处理助手_案例1_文件内容对比.html",
-    "pages/custom/RPA+AI文件处理助手_案例2_表单识别填写.html",
-    "pages/custom/制造业RPA场景地图_PMC部.html",
 
     // 简单实操
     "pages/common/小试影刀.html",
 
-    // 痛点场景案例
-    // "pages/核心场景相关案例1.html",
+    // 核心场景场景案例 - 财务部门
+    "pages/custom/核心场景相关案例2.html",
+    "pages/custom/场景_财务部痛点.html",
+    "pages/custom/场景_财务部方案.html",
 
-    // 核心场景场景案例
+    // 核心场景场景案例 - 电商部门
     "pages/custom/核心场景相关案例1.html",
+
+    // 核心场景场景案例 - 研发部门
+    "pages/custom/其他部门场景相关案例4.html",
+    "pages/custom/BOM物料清单自动化.html",
+    "pages/custom/BOM数据自动化维护.html",
+
+    // 其他部门场景案例 - 生产部门
+    "pages/custom/其他部门场景相关案例1.html",
     "pages/custom/生产日报自动化汇总.html",
     "pages/custom/工单自动化批量管理.html",
     "pages/custom/制造业_文件内容对比.html",
     "pages/custom/制造业_超复杂表单识别.html",
+
+    // 其他部门场景案例 - 计划部门
+    // "pages/custom/制造业_采购月度原料用量计算.html",
+    // "pages/custom/制造业_延期订单查询跟进.html",
+
+    // 其他部门场景案例 - 采购部门
+    "pages/custom/其他部门场景相关案例3.html",
     "pages/custom/制造业_采购每日原材料价格获取.html",
     "pages/custom/制造业_物料到货计划拆分.html",
-    "pages/custom/制造业_采购份额调整分配.html",
-    "pages/custom/制造业_采购月度原料用量计算.html",
-    "pages/custom/制造业_延期订单查询跟进.html",
+    // "pages/custom/制造业_采购份额调整分配.html",
+
+    // 其他部门场景案例 - 销售部门
+    "pages/custom/其他部门场景相关案例2.html",
     "pages/custom/制造业_EDI订单信息提取.html",
     "pages/custom/制造业_OMS销售订单录入.html",
-    "pages/custom/制造业_仓储部出货标签制作.html",
-    "pages/custom/制造业_每日发货流程自动化.html",
+    // "pages/custom/自动化创建销售订单.html",
+    // "pages/custom/采购入库自动化.html",
 
-    // 核心场景场景案例
-    "pages/custom/核心场景相关案例2.html",
-    "pages/custom/BOM物料清单自动化.html",
-    "pages/custom/BOM数据自动化维护.html",
+    // 其他部门场景案例 - 仓储部门
+    // "pages/custom/制造业_仓储部出货标签制作.html",
+    // "pages/custom/制造业_每日发货流程自动化.html",
 
-    // 核心场景场景案例
-    "pages/custom/核心场景相关案例3.html",
-    "pages/custom/自动化创建销售订单.html",
-    "pages/custom/采购入库自动化.html",
-    "pages/custom/ap_rpa_file_assistant.html",
-
-    // 通用场景案例
-    "pages/custom/更多场景案例2.html",
-    "pages/custom/场景_运营部方案.html",
-    "pages/custom/场景_电商客服方案.html",
-    "pages/custom/场景_财务部痛点.html",
-    "pages/custom/场景_财务部方案.html",
+    // "pages/custom/RPA+AI文件处理助手_案例1_文件内容对比.html",
+    // "pages/custom/RPA+AI文件处理助手_案例2_表单识别填写.html",
+    // "pages/custom/制造业RPA场景地图_PMC部.html",
 
     // 课程体系介绍
     "pages/common/从何开始.html",
